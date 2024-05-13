bebf055120333f6d7f737e12ac836ab0dfbc3b7e
Marcos Vinicius <marcos@Marcoss-MacBook-Pro.local>
2024-05-13 07:22:11 -0300

    feat:update installed apps

--- tournament/apps.py
+++ tournament/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TournamentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.tournament'
+    name = 'tournament'
