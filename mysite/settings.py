bf69d9840ad85347bd5850e8795c664c8cde8767
Marcos Vinicius <marcos@Marcoss-MacBook-Pro.local>
2024-05-13 07:46:57 -0300

    feat: add white list origin

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,10 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://django-server-production-050c.up.railway.app"]
+ALLOWED_HOSTS = ["django-server-production-050c.up.railway.app"]
+CORS_ORIGIN_WHITELIST = [
+    'https://django-server-production-050c.up.railway.app',
+]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
