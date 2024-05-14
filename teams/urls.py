0b5c714f6a085cd692ca59f1fc79e54a387f2769
Marcos Vinicius <marcos@Marcoss-MacBook-Pro.local>
2024-05-13 22:08:49 -0300

    feat: allowany to api calls

--- teams/urls.py
+++ teams/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teams/', views.TeamListCreate.as_view(), name='team-list-create'),
-    path('teams/<int:pk>/', views.TeamRetrieveUpdateDestroy.as_view(), name='team-detail'),
+    path('teams/', views.team_list_create.as_view(), name='team-list-create'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,50 +1,40 @@
-from rest_framework import generics, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Team
-from django.contrib.auth.models import User
+from rest_framework import status
+from .models import Team, Player, Tournament
 from .serializers import TeamSerializer
-from tournament.models import Tournament
-from players.models import Player
-from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
 
 
+@api_view(['GET', 'POST'])
+def team_list_create(request):
+    if request.method == 'GET':
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
 
+    elif request.method == 'POST':
+        tournament = get_object_or_404(Tournament, pk=1)
 
-class TeamListCreate(generics.ListCreateAPIView):
-    queryset = Team.objects.all()
-    serializer_class = TeamSerializer
-    permission_classes = (AllowAny,)
-
-
-    def perform_create(self, serializer):
-        serializer.save()
-
-    def post(self, request, *args, **kwargs):
-        tournament = Tournament.objects.get(pk=1)
-
-        if Team.objects.filter(name=request.data['teamName']).first():
+        team_name = request.data.get('teamName')
+        if Team.objects.filter(name=team_name).exists():
             return Response({'error': 'Team already exists'}, status=status.HTTP_400_BAD_REQUEST)
 
-        team = Team.objects.create(name=request.data['teamName'])
+        team = Team.objects.create(name=team_name)
 
-        for player in request.data['players']:
+        players_data = request.data.get('players', [])
+        for player_data in players_data:
             Player.objects.create(
-                first_name=player.get('name', None),
-                birth_date=player.get('dob', None),
-                email=player.get('email', None),
-                phone_number=player.get('phone', None),
-                instagram_user=player.get('instagram', None),
+                first_name=player_data.get('name'),
+                birth_date=player_data.get('dob'),
+                email=player_data.get('email'),
+                phone_number=player_data.get('phone'),
+                instagram_user=player_data.get('instagram'),
                 team=team,
-                is_captain=player.get('is_captain', None),
+                is_captain=player_data.get('is_captain', False),
             )
+
         team.tournament = tournament
         team.save()
-        return Response(status=status.HTTP_201_CREATED)
-
-
-
-class TeamRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Team.objects.all()
-    serializer_class = TeamSerializer
-    permission_classes = (AllowAny,)
-
+        serializer = TeamSerializer(team)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
